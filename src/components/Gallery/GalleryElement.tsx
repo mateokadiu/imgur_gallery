781b4db2359a3d69a6834013123af93089027fdc
Mateo Kadiu <mateo.kadiu@kineton.it>
2023-09-06 19:30:45 +0200

    refactor: added logic to load image cover from images array to avoid unnecessary api calls

--- src/components/Gallery/GalleryElement.tsx
+++ src/components/Gallery/GalleryElement.tsx
@@ -1,52 +1,50 @@
 import React from "react";
 import { ImgurImage } from "../../data/interfaces/imgur.interfaces";
-import { useGetCoverImageQuery } from "../../data/api/imgur.api";
-import { QueryStatus } from "@reduxjs/toolkit/dist/query/react";
 import CardVideo from "../Card/CardVideo";
 import CardImage from "../Card/CardImage";
 import CardContainer from "../Card/CardContainer";
-import LoadingSpinner from "../LoadingSpinner";
-// Define a constant for the background color.
 
 interface GalleryElementProps {
   element: ImgurImage;
 }
 
 const GalleryElement = ({ element }: GalleryElementProps) => {
-  const result = useGetCoverImageQuery(element?.cover);
-
   let content;
 
-  if (result.isSuccess && result.status === QueryStatus.fulfilled) {
-    const source = `http://${result.data.link.split("//")[1]}`;
+  if (element.images && element.images.length > 0) {
+    // If there are multiple images, find the image with id matching the cover property
+    const coverImage = element.images.find(
+      (image) => image.id === element.cover
+    );
 
-    content = (
-      <CardContainer
-        id={element.id}
-        title={element.title}
-        description={{
-          upvotes: element.ups,
-          downvotes: element.downs,
-          views: element.views,
-          score: element.score,
-          commentCount: element.comment_count,
-        }}
-      >
-        {result?.data?.link?.includes("mp4") ? (
-          <CardVideo source={source} />
-        ) : (
-          <CardImage source={source} title={element.title} />
-        )}
-      </CardContainer>
-    );
-  } else if (result.isError) {
-    content = <CardContainer isError={true} />;
+    if (coverImage) {
+      const source = `http://${coverImage.link.split("//")[1]}`;
+      content = (
+        <CardContainer
+          id={element.id}
+          title={element.title}
+          description={{
+            upvotes: element.ups,
+            downvotes: element.downs,
+            views: element.views,
+            score: element.score,
+            commentCount: element.comment_count,
+          }}
+        >
+          {coverImage.type?.includes("video") ? (
+            <CardVideo source={source} />
+          ) : (
+            <CardImage source={source} title={element.title} />
+          )}
+        </CardContainer>
+      );
+    } else {
+      // Handle the case where the cover image is not found within the images array
+      content = <CardContainer isError={true} />;
+    }
   } else {
-    content = (
-      <CardContainer>
-        <LoadingSpinner />
-      </CardContainer>
-    );
+    // If there's only one image or no images, use a simple logic
+    content = <CardContainer />;
   }
 
   return content;
